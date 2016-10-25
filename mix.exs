17c5760af797207de8aa0fd2007bdd532f2ba2d0
Sheharyar Naseer <hello@sheharyar.me>
2016-10-25 19:30:41 +0500

    Added ExUtil as a dependency

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Ecto.Rut.Mixfile do
   defp deps do
     [
       {:ecto,     "~> 2.0.0"},
+      {:ex_utils, "~> 0.1.4"},
       {:postgrex, "~> 0.12.0", only: :test},
       {:ex_doc,   ">= 0.0.0",  only: :dev}
     ]
